ece506df7825bf7bc81961ca998a4e48074eecb5
wenyngcar <owenyoungcarino@gmail.com>
2025-01-08 18:41:37 +0800

    feat: add movie filter

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 # Read the csv file.
 netflix_df = pd.read_csv("netflix_data.csv")
 
-# Subset all the rows with the column duration.
+# Subset the rows with the column duration.
 # Add all the subset rows, then divide with the total no. of rows.
 # Convert into integer.
 duration = int(sum(netflix_df.loc[:, "duration"]) / len(netflix_df))
@@ -14,8 +14,12 @@ print(duration)
 # plt.hist(netflix_df["duration"])
 # plt.savefig("plot.png")
 
-# Subset all the rows with the column duration and genre.
-# Compare the subset duration rows with duration and subset genre if action.
-# Store the length of the result
-short_movie_count = len(netflix_df[np.logical_and(netflix_df["duration"] < duration, netflix_df["genre"] == "Action")])
+# Subset the rows with column type and genre.
+action_movie = np.logical_and(netflix_df["type"] == "Movie", netflix_df["genre"] == "Action")
+
+# Subset the rows with the column duration less than 90 and is a action movie .
+short_action_movie = np.logical_and(action_movie, netflix_df["duration"] < 90)
+
+# Store the length.
+short_movie_count = len(netflix_df[short_action_movie])
 print(short_movie_count)
