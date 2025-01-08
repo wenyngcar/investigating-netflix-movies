12023e75b5a7fed56a42b1f52aefd5fee9d131e2
wenyngcar <owenyoungcarino@gmail.com>
2025-01-08 19:29:10 +0800

    fix: correct subsetting for duration

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 # Read the csv file.
 netflix_df = pd.read_csv("netflix_data.csv")
 
-# Subset the rows with the column duration.
-# Add all the subset rows, then divide with the total no. of rows.
+# Subset the rows with the column type movie and get the duration.
+# Add the total duration of movies divided with the total count of movies.
 # Convert into integer.
-duration = int(sum(netflix_df.loc[:, "duration"]) / len(netflix_df))
+duration = int(sum(netflix_df.loc[netflix_df["type"] == "Movie", "duration"]) / len(netflix_df[netflix_df["type"] == "Movie"]))
 print(duration)
 
 # plt.hist(netflix_df["duration"])
 # plt.savefig("plot.png")
 
 # Subset the rows with column type and genre.
-action_movie = np.logical_and(netflix_df["type"] == "Movie", netflix_df["genre"] == "Action")
+# short_movie_count = netflix_df[netflix_df["type"] == "Movie"][netflix_df["genre"] == "Action"][netflix_df["duration"] < 90]
+# print(short_movie_count)
 
 # Subset the rows with the column duration less than 90 and is a action movie .
-short_action_movie = np.logical_and(action_movie, netflix_df["duration"] < 90)
+# short_action_movie = np.logical_and(action_movie, netflix_df["duration"] < 90)
 
 # Store the length.
-short_movie_count = len(netflix_df[short_action_movie])
-print(short_movie_count)
+# short_movie_count = len(netflix_df[short_action_movie])
+# print(short_movie_count)
